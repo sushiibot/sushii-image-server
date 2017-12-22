82a1e4847511a581c90b1c09050c3228571529d5
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2017-12-21 20:25:17 -0800

    make a new page each api call, then close after screenshot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
@@ -8,27 +9,45 @@ const router = new Router();
 
 async function main() {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({width: 400, height: 300});
 
     router.get('/', (ctx, next) => {
         ctx.body = "hi";
     });
 
-    router.get('/image/:url', async (ctx, next) => {
+    router.get('/url/:url', async (ctx, next) => {
+        const page = await browser.newPage();
+
         await page.goto(ctx.params.url);
         ctx.body = await page.screenshot();
-
-
         ctx.type = 'image/png';
+
+        page.close();
+    });
+
+    router.post('/html', async (ctx, next) => {
+        const page = await browser.newPage();
+
+        const html = ctx.request.body.html;
+        const width = ctx.request.body.width;
+        const height = ctx.request.body.height;
+
+        await page.setViewport({width: width, height: height});
+        await page.setContent(html);
+
+        ctx.body = await page.screenshot();
+        ctx.type = 'image/png';
+
+        page.close();
     });
 
     app
+        .use(bodyParser())
         .use(router.routes())
         .use(router.allowedMethods());
 
 
-    app.listen(3000);
+    app.listen(3000, '127.0.0.1');
+    console.log("Listening on :3000");
 }
 
 
